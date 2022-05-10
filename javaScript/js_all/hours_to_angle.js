483d7821deca3021666e86949da126bc2733ee70
wawandco <wawandco@MBP-de-wawandco.localdomain>
2022-05-10 17:45:35 -0500

    improvements when finding the angles

--- javaScript/js_all/hours_to_angle.js
+++ javaScript/js_all/hours_to_angle.js
@@ -1,29 +1,19 @@
-    
-function change(){
-    document.getElementById("col-1").classList.toggle("col-12")
-    document.getElementById("col-2").classList.toggle("col-12")
-}
-
 document.getElementById("change").onclick=function (){
-    change();
-    
+    document.getElementById("changes").classList.toggle("col-12")
 }
 
 function addText(){
     let hours = document.getElementById("hours").value;
     let minutes = document.getElementById("minutes").value;
-    let hoursText = hours + "Hours"
-    let minutesText = minutes + "Minutes"
-    let text = `At `+ ` ${hoursText.fontcolor('green')} with `+ ` ${minutesText.fontcolor('green')} ` +` there's an angle of:`;
+    let hoursText = hours + " Hours"
+    let minutesText = minutes + " Minutes"
+    let text = `At `+ ` ${hoursText.fontcolor('green')}  with `+ ` ${minutesText.fontcolor('green')} ` +` there's an angle of:`;
 
     if(isNaN(hours) || isNaN(minutes)){
         document.getElementById("read-request").classList.remove("d-none")
-    
     }
     else{
-
         document.getElementById("result-paragraph").innerHTML = text
-
     }
 
 }
@@ -38,9 +28,14 @@ function addText(){
         if(isNaN(hours) || isNaN(minutes)){
             document.getElementById("read-request").classList.remove("d-none")
         }
+        else if(x>=7 & y<30 ){
+            let absolute = (result) + 360
+             document.getElementById("result-angle").innerHTML = absolute  + "°"
+         }
         else if(result < 0){
             document.getElementById("result-angle").innerHTML = result * (-1) + "°"
-        }else{
+        }
+        else{
             document.getElementById("result-angle").innerHTML = result + "°"
         } 
     }
