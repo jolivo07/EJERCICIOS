374296b1ecaf8f24a6e9e9eaaf204c7227b6fc55
wawandco <wawandco@MBP-de-wawandco.localdomain>
2022-05-10 14:16:05 -0500

    add message warning

--- javaScript/js_all/angle_to_hours.js
+++ javaScript/js_all/angle_to_hours.js
@@ -8,18 +8,23 @@ document.getElementById("change").onclick=function (){
     
 }
 
+
 function angle_hours(){
     let hours = document.getElementById("angle-hours").value;
     let minutes = document.getElementById("angle-minutes").value;
     let x = parseInt(hours)
     let y = parseInt(minutes)
-    let resutl = x/30
-   if (hours.length == 0 || (x % 3) != 0 || (y % 6) != 0 || x<30 ){
+    const resutl = x/30
+   if (hours.length == 0 || minutes.length == 0 || x<30 || y<6 ){
     document.getElementById("read-request").classList.remove("d-none")
+   }else if ((x % 3) != 0 || (y % 6) != 0){
+    document.getElementById("read-request").classList.add("d-none")
+    document.getElementById("warning").classList.remove("d-none")
+    document.getElementById("result-hours").innerHTML =Math.trunc(resutl)
    }else{
+    document.getElementById("warning").classList.add("d-none")
     document.getElementById("read-request").classList.add("d-none")
     document.getElementById("result-hours").innerHTML =Math.trunc(resutl)
-     
    }
 
 }
@@ -29,16 +34,23 @@ function angle_minutes(){
     let hours = document.getElementById("angle-hours").value;
     let x = parseInt(hours)
     let y = parseInt(minutes)
-    resutl = y/6
-    if (minutes.length == 0 || (x % 3) != 0 || (y % 6) != 0  || x<30 ){
+    const resutl = y/6
+    if (minutes.length == 0 || hours.length == 0 || y < 6 || x<30 ){
         document.getElementById("read-request").classList.remove("d-none")
-       }else{
+       } else if((y % 6) != 0 ||(x % 3) != 0  ){
+        document.getElementById("warning").classList.remove("d-none")
         document.getElementById("read-request").classList.add("d-none")
         document.getElementById("result-minutes").innerHTML =Math.trunc(resutl)
          
-       }}
+       }else{
+        document.getElementById("warning").classList.add("d-none")
+        document.getElementById("read-request").classList.add("d-none")
+        document.getElementById("result-minutes").innerHTML =Math.trunc(resutl)
+       }
+    }
 
 document.getElementById("btn-send-information").onclick=function(){
    angle_hours()
-   angle_minutes()
+    angle_minutes()
+
 }
